76b345fcf021e4c86f6966784742d6553c12cbeb
Sanaghufran <sanaghufran2005@gmail.com>
2024-08-22 13:00:48 +0530

    As we are using redux useState is removed

--- src/components/Bonus.js
+++ src/components/Bonus.js
@@ -3,11 +3,9 @@ import { useState } from 'react';
 import { incrementBonus } from '../actions';
 
 function Bonus({store}) {
-  const [bonus, setBonus] = useState({ points: 0 });
 
-  const increment = () => {
-    setBonus({ points: bonus.points + 1 });
-  };
+
+ 
   return (
     <div className="card">
       <div className="container">
